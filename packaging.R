94db2f70c859f75ab45069d794146ba4926a622c
mayer79 <michael.mayer@consultag.ch>
2019-07-21 15:39:15 +0200

    CRAN commit

--- packaging.R
+++ packaging.R
@@ -3,6 +3,7 @@
 #=====================================================================================
 
 library(usethis)
+library(devtools)
 
 pkg <- "MetricsWeighted"
 
@@ -30,9 +31,29 @@ proj_activate(path)
 use_roxygen_md()
 
 # Set up other files -------------------------------------------------
-use_readme_md()
-use_news_md()
-#> ✔ Writing 'NEWS.md'
+# use_readme_md()
+# use_news_md()
 
-# Use git ------------------------------------------------------------
-use_git()
+use_package_doc()
+use_cran_comments(path)
+
+# Copy stuff to path
+file.copy(file.path("R", list.files("R")), file.path(path, "R"))
+dir.create(file.path(path, "man"))
+file.copy(file.path("man", list.files("man")), file.path(path, "man"))
+file.copy(c("DESCRIPTION", "NAMESPACE",
+            "NEWS.md", "README.md", "cran-comments.md"), path, overwrite = TRUE)
+file.copy(".Rbuildignore", path, overwrite = TRUE)
+
+# Install the package (locally)
+install(path) # tar
+
+# modify .Rbuildignore in build project to ignore the proj file.
+
+check_win_devel(path)
+
+check_rhub(path)
+
+setwd(file.path("C:/projects/missRanger", pkg))
+
+devtools::release(path)
