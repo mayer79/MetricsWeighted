72d8b874b453c5bbf82ce0390537d55b4d4bcb71
(U118378) <michael.mayer@mobi.ch>
2019-07-23 14:20:45 +0200

    Added some helpful comment on SOmer's D

--- R/AUC.R
+++ R/AUC.R
@@ -1,6 +1,6 @@
 #' Area under the ROC
 #'
-#' @description Weighted AUC, i.e. the area under the receiver operating curve, based on a deterministic version of \code{glmnet::auc}. Note that the unweighted version can be different from the weighted one with unit weights due to ties in "predicted".
+#' @description Weighted AUC, i.e. the area under the receiver operating curve, based on a deterministic version of \code{glmnet::auc}. Note that the unweighted version can be different from the weighted one with unit weights due to ties in \code{predicted}.
 #' @author Michael Mayer, \email{mayermichael79@gmail.com}
 #' @param actual Observed values (0 or 1).
 #' @param predicted Predicted values (not necessarly between 0 and 1).
@@ -8,7 +8,7 @@
 #' @param ... Further arguments passed by other methods.
 #'
 #' @return A numeric vector of length one.
-#' 
+#'
 #' @export
 #'
 #' @examples
@@ -19,9 +19,9 @@
 #' AUC(c(1, 0, 0, 1), c(0.1, 0.2, 0.9, 0.8))
 #' AUC(c(1, 0, 0, 1), c(0.1, 0.2, 0.9, 0.8), w = rep(1, 4)) # same as last (no ties)
 #' AUC(c(0, 0, 1, 1), c(0.1, 0.1, 0.9, 0.8), w = 1:4)
-#' 
+#'
 #' @seealso \code{\link{gini_coefficient}}.
-#' 
+#'
 AUC <- function(actual, predicted, w = NULL, ...) {
   # Modified version of glmnet::auc
   if (is.null(w)) {
@@ -31,7 +31,7 @@ AUC <- function(actual, predicted, w = NULL, ...) {
     u <- sum(r[actual == 1]) - n_pos * (n_pos + 1) / 2
     return(exp(log(u) - log(n_pos) - log(n_neg)))
   }
-  
+
   op <- order(predicted)
   actual <- actual[op]
   w <- w[op]
--- R/gini_coefficient.R
+++ R/gini_coefficient.R
@@ -1,6 +1,6 @@
 #' Gini coefficient
 #'
-#' @description Weighted Gini coefficient, obtained as 2 * AUC - 1.
+#' @description Weighted Gini coefficient, obtained as 2 * AUC - 1. Up to ties in \code{predicted} equivalent to Somer's D.
 #' @author Michael Mayer, \email{mayermichael79@gmail.com}
 #' @param actual Observed values (0 or 1).
 #' @param predicted Predicted values (not necessarly between 0 and 1).
@@ -8,16 +8,16 @@
 #' @param ... Further arguments passed by other methods.
 #'
 #' @return A numeric vector of length one.
-#' 
+#'
 #' @export
 #'
 #' @examples
 #' gini_coefficient(c(0, 0, 1, 1), 2 * c(0.1, 0.1, 0.9, 0.8))
 #' gini_coefficient(c(0, 0, 1, 1), c(0.1, 0.6, 0.9, 0.5))
 #' gini_coefficient(c(0, 0, 1, 1), c(0.1, 0.6, 0.9, 0.5), w = 1:4)
-#' 
+#'
 #' @seealso \code{\link{AUC}}.
-#' 
+#'
 gini_coefficient <- function(actual, predicted, w = NULL, ...) {
   -1 + 2 * AUC(actual = actual, predicted = predicted, w = w, ...)
 }
