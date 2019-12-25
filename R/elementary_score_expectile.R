43943dc1d2ac0969a0acb20dbf781241d91dd7a7
Michael Mayer <mayermichael79@gmail.com>
2019-12-25 09:43:02 +0100

    slight style & roxygen changes

--- R/elementary_score_expectile.R
+++ R/elementary_score_expectile.R
@@ -1,8 +1,8 @@
 #' Elementary Scoring Function for Expectiles
 #'
-#' Weighted average of the elementary scoring function for expectiles at level `alpha` with parameter `theta`, see [1].
-#' Every choice of `theta` gives a scoring function consistent for the expectile at level `alpha`.
-#' Note that the expectile at level `alpha = 0.5` is the expectation (mean).
+#' Weighted average of the elementary scoring function for expectiles at level \code{alpha} with parameter \code{theta}, see [1].
+#' Every choice of \code{theta} gives a scoring function consistent for the expectile at level \code{alpha}.
+#' Note that the expectile at level \code{alpha = 0.5} is the expectation (mean).
 #' The smaller the score, the better.
 #'
 #' @param actual Observed values.
@@ -14,23 +14,26 @@
 #' @return A numeric vector of length one.
 #' @export
 #' @references
-#' [1] Ehm, W., Gneiting, T., Jordan, A. and Krüger, F. (2016), Of quantiles and expectiles: consistent scoring functions, Choquet representations and forecast rankings. J. R. Stat. Soc. B, 78: 505-562. \href{https://doi.org/10.1111/rssb.12154}{doi:10.1111/rssb.12154}
+#' [1] Ehm, W., Gneiting, T., Jordan, A. and Krüger, F. (2016), Of quantiles and expectiles: consistent scoring functions, Choquet representations and forecast rankings. J. R. Stat. Soc. B, 78: 505-562, <doi.org/10.1111/rssb.12154>.
 #' @examples
-#' elemantary_score_expectile(1:10, c(1:9, 12), alpha = 0.5, theta = 1)
-#' elemantary_score_expectile(1:10, c(1:9, 12), alpha = 0.5, theta = 1, w = rep(1, 10))
-elemantary_score_expectile <- function(actual, predicted, w = NULL, alpha = 0.5, theta = 0, ...) {
-  stopifnot(alpha >= 0 && alpha <= 1)
+#' elementary_score_expectile(1:10, c(1:9, 12), alpha = 0.5, theta = 1)
+#' elementary_score_expectile(1:10, c(1:9, 12), alpha = 0.5, theta = 1, w = rep(1, 10))
+elementary_score_expectile <- function(actual, predicted, w = NULL, alpha = 0.5, theta = 0, ...) {
+  stopifnot(length(alpha) == 1L, alpha >= 0, alpha <= 1,
+            length(theta) == 1L,
+            length(actual) == length(predicted))
 
-  score <- abs((actual < predicted) - alpha) *
-    (positive_part(actual - theta) -
-       positive_part(predicted - theta) -
+  score <- abs((actual < predicted) - alpha) * (
+       .positive_part(actual - theta) -
+       .positive_part(predicted - theta) -
        (actual - predicted) * (theta < predicted)
     )
   
   weighted_mean(x = score, w = w, ...)
 }
 
-positive_part <- function(x){
+# Helper function
+.positive_part <- function(x) {
   # slightly faster and less memory than x * (x>=0) and pmax(0, x)
   (x + abs(x))/2
 }
