4b789ea81841b6724239bd956354a7bea2dac203
mayer79 <mayermichael79@gmail.com>
2021-05-22 11:25:12 +0200

    compiled package

--- release/MetricsWeighted/R/r_squared_poisson.R
+++ release/MetricsWeighted/R/r_squared_poisson.R
@@ -2,9 +2,12 @@
 #'
 #' Wrapper to \code{r_squared} with \code{deviance_function = deviance_poisson}.
 #'
+#' The deviance gain is calculated regarding the null model derived from the actual values. While fine for in-sample considerations, this is only an approximation for out-of-sample considerations. There, it is recommended to calculate null deviance regarding the in-sample (weighted) mean. This value can be passed by the argument \code{reference_mean}.
+#'
 #' @param actual Observed values.
 #' @param predicted Predicted values.
 #' @param w Optional case weights.
+#' @param reference_mean An optional reference mean used to derive the null deviance. Recommended in out-of-sample applications.
 #' @param ... Further arguments passed to \code{r_squared}.
 #' @return A numeric vector of length one.
 #' @export
@@ -13,7 +16,9 @@
 #'           deviance_function = deviance_poisson)
 #' r_squared_poisson(0:2, c(0.1, 1, 2), w = rep(1, 3))
 #' @seealso \code{\link{r_squared}}.
-r_squared_poisson <- function(actual, predicted, w = NULL, ...) {
+r_squared_poisson <- function(actual, predicted, w = NULL,
+                              reference_mean = NULL, ...) {
   r_squared(actual = actual, predicted = predicted, w = w,
-            deviance_function = deviance_poisson, ...)
+            deviance_function = deviance_poisson,
+            reference_mean = reference_mean, ...)
 }
