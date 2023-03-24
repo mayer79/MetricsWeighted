8b37a3102b790da0aec9f84f332ad1a836c140fd
Michael Mayer <mayermichael79@gmail.com>
2023-03-24 08:39:28 +0100

    more unit tests

--- tests/testthat/test-stats.R
+++ tests/testthat/test-stats.R
@@ -6,6 +6,18 @@ test_that("unweighted stats match unweighted function", {
   expect_equal(quantile(x, probs = 0.2), weighted_quantile(x, probs = 0.2))
   expect_equal(var(x), weighted_var(x))
   expect_equal(cor(x, y), weighted_cor(x, y))
+
+  # Now with missing values
+  x <- c(0, 1, 2, 1, 2, 3, 4, NA)
+  y <- c(NA, 2:length(x))
+  expect_equal(mean(x, na.rm = TRUE), weighted_mean(x, na.rm = TRUE))
+  expect_equal(median(x, na.rm = TRUE), weighted_median(x, na.rm = TRUE))
+  expect_equal(
+    quantile(x, probs = 0.2, na.rm = TRUE),
+    weighted_quantile(x, probs = 0.2, na.rm = TRUE)
+  )
+  expect_equal(var(x, na.rm = TRUE), weighted_var(x, na.rm = TRUE))
+  expect_equal(cor(x, y, use = "p"), weighted_cor(x, y, na.rm = TRUE))
 })
 
 test_that("weight 1 gives same as unweighted", {
@@ -18,6 +30,30 @@ test_that("weight 1 gives same as unweighted", {
                weighted_quantile(x, probs = 0.2, w = w))
   expect_equal(weighted_var(x), weighted_var(x, w = w))
   expect_equal(weighted_cor(x, y), weighted_cor(x, y, w = w))
+
+  # Now with missing values
+  x <- c(0, 1, 2, 1, 2, 3, 4, NA)
+  y <- c(NA, 2:length(x))
+  expect_equal(
+    weighted_mean(x, na.rm = TRUE),
+    weighted_mean(x, w = w, na.rm = TRUE)
+  )
+  expect_equal(
+    weighted_median(x, na.rm = TRUE),
+    weighted_median(x, w = w, na.rm = TRUE)
+  )
+  expect_equal(
+    weighted_quantile(x, probs = 0.2, na.rm = TRUE),
+    weighted_quantile(x, probs = 0.2, w = w, na.rm = TRUE)
+  )
+  expect_equal(
+    weighted_var(x, na.rm = TRUE),
+    weighted_var(x, w = w, na.rm = TRUE)
+  )
+  expect_equal(
+    weighted_cor(x, y, na.rm = TRUE),
+    weighted_cor(x, y, w = w, na.rm = TRUE)
+  )
 })
 
 test_that("weight 1 gives same as weight 2", {
@@ -31,6 +67,30 @@ test_that("weight 1 gives same as weight 2", {
                weighted_quantile(x, probs = 0.2, w = w1))
   expect_equal(weighted_var(x, w = w2), weighted_var(x, w = w1))
   expect_equal(weighted_cor(x, y, w = w2), weighted_cor(x, y, w = w1))
+
+  # Now with missing values
+  x <- c(0, 1, 2, 1, 2, 3, 4, NA)
+  y <- c(NA, 2:length(x))
+  expect_equal(
+    weighted_mean(x, w = w2, na.rm = TRUE),
+    weighted_mean(x, w = w1, na.rm = TRUE)
+  )
+  expect_equal(
+    weighted_median(x, w = w2, na.rm = TRUE),
+    weighted_median(x, w = w1, na.rm = TRUE)
+  )
+  expect_equal(
+    weighted_quantile(x, w = w2, probs = 0.2, na.rm = TRUE),
+    weighted_quantile(x, probs = 0.2, w = w1, na.rm = TRUE)
+  )
+  expect_equal(
+    weighted_var(x, w = w2, na.rm = TRUE),
+    weighted_var(x, w = w1, na.rm = TRUE)
+  )
+  expect_equal(
+    weighted_cor(x, y, w = w2, na.rm = TRUE),
+    weighted_cor(x, y, w = w1, na.rm = TRUE)
+  )
 })
 
 test_that("non-constant weights have an effect", {
@@ -43,5 +103,24 @@ test_that("non-constant weights have an effect", {
                weighted_quantile(x, probs = 0.2, w = w))
   expect_false(weighted_var(x) == weighted_var(x, w = w))
   expect_false(weighted_cor(x, y) == weighted_cor(x, y, w = w))
+
+  # Now with missing values
+  x <- c(0, 1, 2, 1, 2, 3, 4, 5, NA)
+  y <- c(NA, 2:length(x))
+  w <- 1:length(x)
+  expect_false(
+    weighted_mean(x, na.rm = TRUE) == weighted_mean(x, w = w, na.rm = TRUE)
+  )
+  expect_false(
+    weighted_median(x, na.rm = TRUE) == weighted_median(x, w = w, na.rm = TRUE)
+  )
+  expect_false(
+    weighted_quantile(x, probs = 0.2, na.rm = TRUE) ==
+    weighted_quantile(x, probs = 0.2, w = w, na.rm = TRUE)
+  )
+  expect_false(weighted_var(x, na.rm = TRUE) == weighted_var(x, w = w, na.rm = TRUE))
+  expect_false(
+    weighted_cor(x, y, na.rm = TRUE) == weighted_cor(x, y, w = w, na.rm = TRUE)
+  )
 })
 
